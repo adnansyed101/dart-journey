8d6643d33fc9c92b744917889fd64bfa158b6f7d
adnansyed101 <sirgeant10@gmail.com>
2026-01-18 15:30:40 +0600

    Add extra code.

--- bin/dart_journey.dart
+++ bin/dart_journey.dart
@@ -1,3 +1,63 @@
+import 'dart:io';
+import "package:http/http.dart" as http;
+
+const version = "0.0.1";
+
 void main(List<String> arguments) {
-  print("hello Dart");
+  if (arguments.isEmpty || arguments.first == "help") {
+    printUsage();
+  } else if (arguments.first == "version") {
+    print("Cli version is --> $version");
+  } else if (arguments.first == "wikipedia") {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
+}
+
+void searchWikipedia(List<String>? arguments) async {
+  late String? articleTitle;
+
+  if (arguments == null || arguments.isEmpty) {
+    print("Please provide an article title");
+    final inputFromStdin = stdin.readLineSync();
+
+    if (inputFromStdin == null || inputFromStdin.isEmpty) {
+      print("No article title provided. Exiting search.");
+      return;
+    }
+    articleTitle = inputFromStdin;
+  } else {
+    articleTitle = arguments.join(" ");
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+
+  // Call the API and await the result
+  var articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent); // Print the full article response (raw JSON for now)
+}
+
+void printUsage() {
+  // Add this new function
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
+}
+
+Future<String> getWikipediaArticle(String articleTitle) async {
+  final client = http.Client();
+  final uri = Uri.https(
+    'en.wikipedia.org', // Wikipedia API domain
+    '/api/rest_v1/page/summary/$articleTitle', // API path for article summary
+  );
+
+  final response = await client.get(uri);
+
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+
+  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
 }
